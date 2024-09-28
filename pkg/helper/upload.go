9ad99def94841e9602230bfd09052178613ea0f3
Akrom <akrom@gmail.com>
2024-09-28 13:24:52 +0500

    minor changes

--- pkg/helper/upload.go
+++ pkg/helper/upload.go
@@ -20,32 +20,25 @@ import (
 func UploadFiles(file *multipart.Form) (*models.MultipleFileUploadResponse, error) {
 	var resp models.MultipleFileUploadResponse
 
-	// Path to your Firebase service account key file
 	filePath := filepath.Join("./", "serviceAccountKey.json")
-
-	// Initialize Firebase App with service account key
 	opt := option.WithCredentialsFile(filePath)
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Println("Firebase App initialization error:", err)
 		return nil, err
 	}
-
-	// Initialize Firebase Storage client
 	client, err := app.Storage(context.Background())
 	if err != nil {
 		log.Println("Firebase Storage client initialization error:", err)
 		return nil, err
 	}
 
-	// Get a handle to the Firebase Storage bucket
 	bucketHandle, err := client.Bucket("food-8ceb4.appspot.com")
 	if err != nil {
 		log.Println("Bucket handle error:", err)
 		return nil, err
 	}
 
-	// Iterate over the uploaded files
 	for _, v := range file.File["file"] {
 		id := uuid.New().String()
 		imageFile, err := v.Open()
@@ -53,11 +46,10 @@ func UploadFiles(file *multipart.Form) (*models.MultipleFileUploadResponse, erro
 			log.Println("Error opening file:", v.Filename, err)
 			return nil, err
 		}
-		defer imageFile.Close() // Ensure file is closed after processing
+		defer imageFile.Close() 
 
 		log.Println("Uploading file:", v.Filename)
 
-		// Upload the file to Firebase Storage
 		objectHandle := bucketHandle.Object(v.Filename)
 		writer := objectHandle.NewWriter(context.Background())
 		writer.ObjectAttrs.Metadata = map[string]string{"firebaseStorageDownloadTokens": id}
@@ -67,7 +59,6 @@ func UploadFiles(file *multipart.Form) (*models.MultipleFileUploadResponse, erro
 			return nil, err
 		}
 
-		// Closing the writer to complete the upload
 		if err := writer.Close(); err != nil {
 			log.Printf("Error closing writer for file %s: %v", v.Filename, err)
 			return nil, err
@@ -75,11 +66,9 @@ func UploadFiles(file *multipart.Form) (*models.MultipleFileUploadResponse, erro
 
 		log.Println("File uploaded successfully:", v.Filename)
 
-		// Encode the file name to handle spaces and special characters
 		encodedFileName := url.PathEscape(v.Filename)
 		fileURL := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/food-8ceb4.appspot.com/o/%s?alt=media&token=%s", encodedFileName, id)
 
-		// Append the file URL to the response
 		resp.Url = append(resp.Url, &models.Url{
 			Id:  id,
 			Url: fileURL,
@@ -89,7 +78,6 @@ func UploadFiles(file *multipart.Form) (*models.MultipleFileUploadResponse, erro
 	return &resp, nil
 }
 
-// DeleteFile deletes a file from Firebase Storage
 func DeleteFile(id string) error {
 	ctx := context.Background()
 	client, err := storage.NewService(ctx, option.WithCredentialsFile("serviceAccountKey.json"))
